3cbbc2843c11909be6e2008147f894affbf9ec50
Leon Rosenberg <leon.rosenberg@anotheria.net>
2013-06-09 11:44:24 +0200

    added test for file and example file

--- test/junit/org/moskito/controlagent/AgentConfigTest.java
+++ test/junit/org/moskito/controlagent/AgentConfigTest.java
@@ -1,8 +1,10 @@
 package org.moskito.controlagent;
 
+import org.configureme.ConfigurationManager;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class AgentConfigTest{
@@ -13,6 +15,18 @@ public class AgentConfigTest{
 		assertEquals(0, config.getIncludedProducersList().size());
 		assertEquals(0, config.getExcludedProducersList().size());
 
+	}
+
+	@Test public void testFileConfig(){
+		AgentConfig config = new AgentConfig();
+		ConfigurationManager.INSTANCE.configure(config);
+		assertFalse(config.includeAll());
+
+		assertTrue(config.getIncludedProducersList().contains("Foo"));
+		assertEquals(3, config.getIncludedProducersList().size());
+
+		assertFalse(config.getExcludedProducersList().contains("Foo"));
+		assertEquals(1, config.getExcludedProducersList().size());
 	}
 
 
