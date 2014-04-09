7ad09ebf599dd57e8fc699ecb79b63ed483a5943
Leon Rosenberg <leon.rosenberg@anotheria.net>
2014-04-09 16:04:46 +0200

    initial agent implementation

--- rmi-endpoint/java/org/moskito/controlagent/endpoints/rmi/AgentService.java
+++ rmi-endpoint/java/org/moskito/controlagent/endpoints/rmi/AgentService.java
@@ -1,21 +1,48 @@
 package org.moskito.controlagent.endpoints.rmi;
 
 import net.anotheria.anoprise.metafactory.Service;
+import org.moskito.controlagent.data.accumulator.AccumulatorHolder;
+import org.moskito.controlagent.data.accumulator.AccumulatorListItem;
 import org.moskito.controlagent.data.status.ThresholdStatusHolder;
 import org.moskito.controlagent.data.threshold.ThresholdDataItem;
 
 import java.util.List;
+import java.util.Map;
 
 /**
- * TODO comment this class
+ * AgentService is the base for distribution and a distributeme wrapper for the Agent.
  *
  * @author lrosenberg
  * @since 09.04.14 13:50
  */
 public interface AgentService extends Service{
-	public ThresholdStatusHolder getThresholdStatus();
+	/**
+	 * Returns the current status.
+	 * @return
+	 * @throws AgentServiceException
+	 */
+	ThresholdStatusHolder getThresholdStatus() throws AgentServiceException;
 
-	public List<ThresholdDataItem> getThresholds();
+	/**
+	 * Returns the list of contained thresholds.
+	 * @return
+	 * @throws AgentServiceException
+	 */
+	List<ThresholdDataItem> getThresholds() throws AgentServiceException;
 
+	/**
+	 * Returns the list of available accumulators.
+	 * @return
+	 * @throws AgentServiceException
+	 */
+	List<AccumulatorListItem> getAvailableAccumulators() throws AgentServiceException;
+
+	/**
+	 * Returns accumulator data
+	 * @param accumulatorNames
+	 * @return
+	 * @throws AgentServiceException
+	 */
+	Map<String, AccumulatorHolder> getAccumulatorsData(List<String> accumulatorNames) throws AgentServiceException;
 
 }
--- rmi-endpoint/java/org/moskito/controlagent/endpoints/rmi/AgentServiceException.java
+++ rmi-endpoint/java/org/moskito/controlagent/endpoints/rmi/AgentServiceException.java
@@ -6,5 +6,12 @@ package org.moskito.controlagent.endpoints.rmi;
  * @author lrosenberg
  * @since 09.04.14 14:02
  */
-public class AgentServiceException {
+public class AgentServiceException extends Exception{
+ 	public AgentServiceException(String message){
+		super(message);
+	}
+
+	public AgentServiceException(String message, Throwable cause){
+		super(message, cause);
+	}
 }
