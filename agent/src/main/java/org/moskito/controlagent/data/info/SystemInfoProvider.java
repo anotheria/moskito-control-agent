e138c7d369da49c56c96ae43d09804c838781b54
Satan The <submitthesatan666@gmail.com>
2017-06-23 13:38:43 +0300

    edited comments in SystemInfoProvider class

--- agent/src/main/java/org/moskito/controlagent/data/info/SystemInfoProvider.java
+++ agent/src/main/java/org/moskito/controlagent/data/info/SystemInfoProvider.java
@@ -6,23 +6,32 @@ import java.lang.management.ManagementFactory;
 import java.util.Map;
 
 /**
- * TODO : COMMENT THIS CLASS
+ * Singleton, that provides
+ * information about monitored application.
+ * Contains one public method {@link SystemInfoProvider#getSystemInfo()}
+ * that returns {@link SystemInfo} instance with collected data.
  */
 public class SystemInfoProvider {
 
+    /**
+     * Holds SystemInfoProvider instance
+     */
     private static final SystemInfoProvider INSTANCE = new SystemInfoProvider();
 
+    /**
+     * Most of information fields is not changing
+     * since app launch. This var holds {@link SystemInfo}
+     * instance with this information.
+     * Later copy of this object with
+     */
     private SystemInfo initialInfo;
 
+    /**
+     * Constructor, that builds initial info
+     * object, filling java version, app launch command and
+     * machine name.
+     */
     private SystemInfoProvider(){
-        initialInfo = buildInitialSystemInfo();
-    }
-
-    public static SystemInfoProvider getInstance(){
-        return INSTANCE;
-    }
-
-    private SystemInfo buildInitialSystemInfo(){
 
         initialInfo = new SystemInfo();
 
@@ -36,16 +45,37 @@ public class SystemInfoProvider {
                 getStartCommand()
         );
 
-        return initialInfo;
-
     }
 
+    public static SystemInfoProvider getInstance(){
+        return INSTANCE;
+    }
+
+    /**
+     * Returns {@link SystemInfo} object,
+     * that contains data about this monitored app.
+     *
+     * Builds new {@link SystemInfo} from existing one:
+     * this class singleton object has field with
+     * {@link SystemInfo} instance, that already
+     * contains some data occurred on class initialization.
+     * Actually, only uptime updates here.
+     *
+     * @return {@link SystemInfo} filled with corresponding data
+     */
     public SystemInfo getSystemInfo(){
         SystemInfo info = new SystemInfo(initialInfo);
         info.setUptime(getUptime());
         return info;
     }
 
+    /**
+     * Returns start command of this application.
+     * Some data may not to be collected due
+     * security settings and jvm  version.
+     * TODO : IMPROVE THIS METHOD
+     * @return start command of this app
+     */
     private String getStartCommand(){
 
         String command;
@@ -54,7 +84,8 @@ public class SystemInfoProvider {
 
         try{
 
-            try {
+            try { // trying to get start command using sun.java.command
+                  // Work of this code may not to be guarantee on OpenJDK
                 command = System.getProperty("sun.java.command");
             }
             catch (SecurityException| IllegalArgumentException e){
@@ -62,19 +93,26 @@ public class SystemInfoProvider {
             }
 
             classpath = "-cp " + System.getProperty("java.class.path");
+
+            // Getting start command arguments and joining it to single string
             arguments = StringUtils.join(
                             ManagementFactory.getRuntimeMXBean().getInputArguments(),
                             " "
                         );
+
         }
         catch (SecurityException| IllegalArgumentException e){
             return "Failed to get run command info due " + e.getClass().getName()
                     + ": " + e.getMessage();
         }
 
-        return command + " " + classpath + " " + arguments;
+        return "java " + command + " " + classpath + " " + arguments;
     }
 
+    /**
+     * Returns machine name, where this app is launched
+     * @return current machine name
+     */
     private String getMachineName()
     {
         Map<String, String> env = System.getenv();
@@ -88,11 +126,25 @@ public class SystemInfoProvider {
             return "Unknown Computer";
     }
 
+    /**
+     * Returns string with
+     * this application uptime in format:
+     *
+     * {DAYS},{HOURS},{MINUTES},{SECONDS}
+     *
+     * All numbers do not contain leading zeros
+     *
+     * @return string that represents this app uptime
+     */
     private String getUptime(){
 
+        // Retrieving milliseconds timestamp from java management framework
         long uptimeTimestamp = ManagementFactory.getRuntimeMXBean().getUptime();
         StringBuilder uptime = new StringBuilder();
 
+        // builtin DateTime formatters do
+        // not used here, because the return day of the year.
+        // this may cause zeroing of days in time string
         final long SECOND = 1000;
         final long MINUTE = SECOND * 60;
         final long HOUR = MINUTE * 60;
